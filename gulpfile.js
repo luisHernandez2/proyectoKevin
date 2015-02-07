ec9422ad99a7f4308cbb4f096ade303522f69ade
luisHernandez2 <lmhernandez21187@gmail.com>
2015-02-07 15:19:17 -0500

    3
    nos mudamos x q lucho....

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+/*
+* Dependencias
+*/
+var gulp = require('gulp'),
+	jshint = require('gulp-jshint'),
+	concat = require('gulp-concat'),
+	stylus = require('gulp-stylus'),
+	nib = require('nib'),
+	watch = require('gulp-watch'),
+	uglify = require('gulp-uglify');
+
+/*
+* Configuración de la tarea 'demo'
+*/
+gulp.task('jsgeneral', function () {
+	gulp.src('js/source/generales/*.js')
+		.pipe(jshint())//revisa los javascrip a ver si hay error
+		.pipe(jshint.reporter('default'))//manda error en consola
+		.pipe(concat('jsgeneral.min.js'))//concatena
+		.pipe(uglify())//minimifica
+		.pipe(gulp.dest('js/build/'))//lo ponemos en la carpeta build
+});
+/**/
+gulp.task('nib', function(){//mi tarea se llaamra nib
+    gulp.src('./css/style.styl')//entradas stylus, y le decimos que use la libreria nib
+        .pipe(stylus({ use: [nib()], compress: true }))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('css/style.styl', ['nib','jsgeneral']);
+});
+/*
+js/source/alone.js
+js/source/[asterisco].js
+js/[Asterisco][Asterisco]/[Asterisco].js coincide con los archivos que terminen en .js dentro de la carpeta js y dentro de todas sus sub-carpetas
+!js/source/3.js Excluye especificamente el archivo 3.js.
+static/*.+(js|css) coincide con los archivos que terminen en .js ó .css
+/* 
+Se define la tarea estaticos, la cual corra otras 3 tareas
+gulp.task('estaticos', ['imagenes', 'css', 'js']);
+*/
+
+/*
+Ejecutar la tarea css cuando la tarea imagenes haya terminado
+gulp.task('css', ['imagenes'], function () {...});
+*/
+
+/*
+definimos una tarea por defecto
+gulp.task('default', function () {...});
+o que una tarea por defecto sea una lista de tareas
+gulp.task('default', ['css', 'js']);
+para ejecutar ponemos en la terminal: gulp
+*/
+
+/*Cada vez que se modifique un file dentro de source se ejecutará la tarea js*/
+//gulp.watch('js/source/*.js', ['js']);
+ /*gulp.watch('js/source/*.js', function(){
+ aquí va el cod a ejecutar, o también se podría ejecutar una tarea mediante el metodo gulp.start('miTarea')}); */
--- index.php
+++ index.php
@@ -10,7 +10,56 @@
       <?php include('include/meta-social.html'); ?>
   </head>
   <body>
+  	<section class="slidemain">
+  		<ul class="slide_list" id="slidesServicio">
+  			<li class="slide_item" style="background-image:url('img/img1.jpg')">
+  				<div class="contenidoSlide">
+  					<div class="titulo_slide"></div>
+  					<a href="" class="btnCalltoAction"></a>
+  				</div>
+  			</li>
+  			<li class="slide_item" style="background-image:url('img/img2.jpg')">
+  				<div class="contenidoSlide">
+  					<div class="titulo_slide"></div>
+  					<a href="" class="btnCalltoAction"></a>
+  				</div>
+  			</li>
+  			<li class="slide_item" style="background-image:url('img/img3.jpg')">
+  				<div class="contenidoSlide">
+  					<div class="titulo_slide"></div>
+  					<a href="" class="btnCalltoAction"></a>
+  				</div>
+  			</li>
+  			<li class="slide_item" style="background-image:url('img/img4.jpg')">
+  				<div class="contenidoSlide">
+  					<div class="titulo_slide"></div>
+  					<a href="" class="btnCalltoAction"></a>
+  				</div>
+  			</li>
+  		</ul>
+  	</section>
   	<?php include('include/footer.html'); ?>
   	<?php include('include/js-general.html'); ?>    
+    <script>
+    	$document.on("ready",function() {
+    		$('#slideServicio').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    responsiveClass:true,
+		    mouseDrag:true,
+		    touchDrag:false,
+		    animateIN: 'fadeIn',
+		    animateOut: 'fadeOut',
+		    autoplay: false,
+		    autoplayTimeout: 3000,
+		    responsive:{
+		        0:{
+		            items:1,
+		            nav:true
+		        }
+    			}
+			})
+    	})	
+    </script>
     </body>
 </html>
